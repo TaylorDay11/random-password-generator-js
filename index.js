8e82fa1d1d2b837c3334d7bc31968761cf1a519c
Taylor Hershberger <taylor.day.hershberger@gmail.com>
2024-10-04 21:53:11 -0600

    Uploaded files

---
+++ index.js
@@ -0,0 +1,79 @@
+let characters = [];
+
+const noSymbolAndNumbersCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const noSymbolsCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const noNumbersCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+const allCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+let noNumbers = false
+let noNumbersButton = document.getElementById("no-numbers")
+let noSymbols = false
+let noSymbolsButton = document.getElementById("no-symbols")
+let passwordLength = 15
+let generatePasswords = document.getElementById("generate-passwords")
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+
+noNumbersButton.addEventListener("click", function(){
+    if(noNumbers === false){
+        noNumbers = true
+        noNumbersButton.classList.add("selected")
+    }else{
+        noNumbers = false
+        noNumbersButton.classList.remove("selected")
+    }
+})
+
+noSymbolsButton.addEventListener("click", function(){
+    if(noSymbols === false){
+        noSymbols = true
+        noSymbolsButton.classList.add("selected")
+    }else{
+        noSymbols = false
+        noSymbolsButton.classList.remove("selected")
+    }
+})
+
+function selectedCharacters(){
+    characters = []
+    if (noSymbols === false && noNumbers === false){
+        characters = allCharacters
+    } else if(noSymbols === false && noNumbers === true){
+        characters = noNumbersCharacters
+    } else if(noSymbols === true && noNumbers === false){
+        characters = noSymbolsCharacters
+    } else if(noSymbols === true && noNumbers === true){
+        characters = noSymbolAndNumbersCharacters
+    }
+}
+
+function limitCharacters(){
+   let limitCharactersValue = document.getElementById("password-length").value
+   if(limitCharactersValue === ""){
+        passwordLength = 15
+    } else if(limitCharactersValue <= 15){
+        passwordLength = limitCharactersValue
+    }
+    else{
+        passwordLength = 15
+    }
+}
+
+generatePasswords.addEventListener("click", function(){
+   passwordOne.textContent = ""
+   passwordTwo.textContent = ""
+   selectedCharacters()
+   limitCharacters() 
+    for(i = 0; i < passwordLength; i++){
+        let randomNumber = Math.floor(Math.random() * characters.length)
+        passwordOne.textContent += characters[randomNumber]
+    }
+    for(i = 0; i < passwordLength; i++){
+        let randomNumber = Math.floor(Math.random() * characters.length)
+        passwordTwo.textContent += characters[randomNumber]
+    }
+})
+
